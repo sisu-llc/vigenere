6a6ffda68d1876f678eb4fa6fec265a56b65a7c7
Dave Voutila <voutilad@gmail.com>
2019-08-18 20:03:43 -0400

    Use args to specify file
    Also start playing with some "striping" logic that will sample every
    n-th character as needed for some key size detection.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,56 @@
-// vigenere
+// vigenere cipher breaking fun for Coursera Cryptography class
+use std::env;
 use std::fs;
+use std::io;
 
-//const INPUT: &str = "/home/dave/src/coursera/cryptography/vigenere/input/cipher.txt";
-const INPUT: &str = "/home/dave/src/coursera/cryptography/vigenere/input/test.txt";
+fn parse(file: &str) -> Result<Vec<u8>, io::Error> {
+    let s = fs::read_to_string(file)?;
 
-fn main() {
-    let s = fs::read_to_string(INPUT)
-        .expect(format!("Could not read {:?}", INPUT).as_str());
-    println!("{}", s);
-
-    let r: Vec<u8> = (1..s.len() as u8).into_iter().filter(|i| i % 2 == 0).collect();
+    let r: Vec<u8> = (1..s.len() as u8)
+        .filter(|i| i % 2 == 0)
+        .collect();
 
     let mut chars = s.chars();
+    let mut result: Vec<u8> = Vec::with_capacity(r.len());
+
     for _ in r {
         let a = chars.next().unwrap();
         let b = chars.next().unwrap();
         let mut chunk = String::new();
         chunk.push(a);
         chunk.push(b);
+
         let int = u8::from_str_radix(chunk.as_str(), 16).unwrap();
-        println!("a: {}, b: {}, chunk: 0x{:x?}, int: {}, char: {}",
-                 a, b, int, chunk, int as char);
+        result.push(int);
+    }
+    Ok(result)
+}
+
+fn stripe<T: Copy>(v: Vec<T>, n: usize) -> Vec<T> {
+    let mut result: Vec<T> = Vec::with_capacity((v.len() / n) + 1 as usize);
+    let idx: Vec<usize> = (1..v.len()).filter(|i| i % n == 0).collect();
+
+    for i in idx {
+        result.push(v[i]);
+    }
+    result
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        println!("Usage: [filename] [stripe index]");
+        return;
+    }
+
+    let file = &args[1];
+    let n = usize::from_str_radix(&args[2], 10).expect("Invalid usize value!");
+
+    match parse(file) {
+        Ok(data) => {
+            println!("{:?}", stripe(data, n));
+        }
+        Err(e) => println!("Error!: {}", e),
     }
 }
